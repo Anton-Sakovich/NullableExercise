1741830b091013f8cadb7b3c5e3d123ad545def2
Anton Sakovich <sakovich.2.718281828459045@gmail.com>
2019-04-01 13:18:58 +0300

    Add AmateurNullable and AmateurNullableTests

---
+++ NullableExercise.Tests/AmateurNullableTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NullableExercise.Tests
+{
+    [TestClass]
+    public class AmateurNullableTests
+    {
+        [TestMethod]
+        public void InitializationTests()
+        {
+            AmateurNullable<int> a = null;
+
+            Assert.IsFalse(a.HasValue);
+            Assert.ThrowsException<InvalidOperationException>(() => a.Value);
+
+            a = 1;
+
+            Assert.IsTrue(a.HasValue);
+            Assert.AreEqual(a.Value, 1);
+        }
+
+        [TestMethod]
+        public void ExplicitConversion_BackToValue_Tests()
+        {
+            AmateurNullable<int> a = null;
+
+            Assert.ThrowsException<InvalidOperationException>(() => { int b = (int)a; });
+
+            a = 1;
+
+            Assert.AreEqual((int)a, 1);
+        }
+    }
+}
---
+++ NullableExercise/AmateurNullable.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullableExercise
+{
+    public struct AmateurNullable<T> where T : struct
+    {
+        private T _Value;
+        private bool _HasValue;
+
+        public AmateurNullable(T val)
+        {
+            _Value = val;
+            _HasValue = true;
+        }
+
+        public T Value
+        {
+            get
+            {
+                if(_HasValue)
+                {
+                    return _Value;
+                }
+                else
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+            set
+            {
+                _Value = value;
+                _HasValue = true;
+            }
+        }
+
+        public bool HasValue
+        {
+            get => _HasValue;
+        }
+
+        public static implicit operator AmateurNullable<T>(AmateurNullable target)
+        {
+            return new AmateurNullable<T>();
+        }
+
+        public static implicit operator AmateurNullable<T>(T val)
+        {
+            return new AmateurNullable<T>(val);
+        }
+
+        public static explicit operator T(AmateurNullable<T> val)
+        {
+            if(val.HasValue)
+            {
+                return val.Value;
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+
+    // This is a placeholder class, which is necessary to be able
+    // to cast null to AmateurNullable<T>. When an implicit cast of
+    // null is determined, the proram will resolve to
+    // operator AmateurNullable<T>(AmateurNullable target)
+    public sealed class AmateurNullable
+    {
+        
+    }
+}
